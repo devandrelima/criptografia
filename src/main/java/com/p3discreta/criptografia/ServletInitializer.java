882ce97ff601c55c6636e5803c25fa61b1062965
devAndreLA <andre.alves@alunos.ufersa.edu.br>
2024-10-18 20:53:40 -0300

    first commit

---
+++ src/main/java/com/p3discreta/criptografia/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.p3discreta.criptografia;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(CriptografiaApplication.class);
+	}
+
+}
---
+++ src/test/java/com/p3discreta/criptografia/CriptografiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.p3discreta.criptografia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CriptografiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
