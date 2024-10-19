fa449f06d1e355b09b87cca4d162f478e8f7e95d
devAndreLA <andre.alves@alunos.ufersa.edu.br>
2024-10-19 20:04:33 -0300

    mudanças atualizadas

--- src/main/java/com/p3discreta/criptografia/model/MaquinaDescriptografar.java
+++ src/main/java/com/p3discreta/criptografia/model/MaquinaDescriptografar.java
@@ -1,11 +1,23 @@
 package com.p3discreta.criptografia.model;
 
+import com.p3discreta.criptografia.repository.LetraNum;
+
 public class MaquinaDescriptografar {
 
-    
-
     public String descriptografarEmPrimeiroGrau(String texto, int a, int b){
-        String textoDesriptografado = "texto descriptografado em 1 grau";
+        String textoDesriptografado = "";
+        texto = texto.toUpperCase();
+        int index;
+        int newIndex;
+
+        for(int i = 0; i < texto.length(); i++){
+            index = LetraNum.alfabeto.indexOf(texto.charAt(i)); // Vê qual letra do texto corresponde a qual número do alfabeto, por exemplo, A = 0
+            newIndex = ((index%28)-b)/a; // criptografa
+
+           
+
+            textoDesriptografado += LetraNum.alfabeto.charAt(newIndex);
+        }
 
         return textoDesriptografado;
     }
