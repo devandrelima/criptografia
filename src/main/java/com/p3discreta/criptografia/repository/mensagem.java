4fa4efd4ef61e37717396e1636dc1122ad16e79f
devAndreLA <andre.alves@alunos.ufersa.edu.br>
2024-10-21 19:34:31 -0300

    alguns comentários adicionados

--- src/main/java/com/p3discreta/criptografia/repository/mensagem.java
+++ src/main/java/com/p3discreta/criptografia/repository/mensagem.java
@@ -1,3 +1,4 @@
 package com.p3discreta.criptografia.repository;
 
+// Utilizado como molde para enviar json e receber do front
 public record mensagem(String texto, int id, int a, int b, int c, Integer[] textoEmNumero) {}
--- src/main/java/com/p3discreta/criptografia/repository/mensagemInterna.java
+++ src/main/java/com/p3discreta/criptografia/repository/mensagemInterna.java
@@ -1,3 +1,4 @@
 package com.p3discreta.criptografia.repository;
 
+// Utilizado apenas internamente no código
 public record mensagemInterna(String texto, Integer[] textoEmNumero) {}
